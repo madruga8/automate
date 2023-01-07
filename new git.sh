935d89e1bce00b380c13e9601b0b1b2149c6340c
madruga <madruga@madruga.com>
2023-01-07 09:52:15 -0300

    asd

---
+++ new git.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Enter the URL of the existing repository: "
+read repo_url
+
+echo "Enter the URL of the new repository (without .git): "
+read new_repo_url
+
+echo "Cloning existing repository..."
+git clone $repo_url
+
+cd $repo_name
+
+echo "Removing existing Git configuration..."
+rm -rf .git
+
+echo "Initializing new Git repository..."
+git init
+
+echo "Adding files to new repository..."
+git add .
+
+echo "Committing changes..."
+git commit -m "Initial commit"
+
+echo "Adding new remote repository..."
+git remote add origin $new_repo_url.git
+
+echo "Pushing changes to new repository..."
+git push -u origin main
+
+echo "Done!"
